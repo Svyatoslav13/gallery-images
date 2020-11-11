e0b93ee668db44aedf591c6a735e526c59100f15
Svyatoslav <pyrokiness@mail.ru>
2020-11-11 21:11:37 +0300

    Add gallery and output all comments

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('URL', 'http://first_php_project.com/module17_practice'); // URL текущей страницы
+define('UPLOAD_MAX_SIZE', 1048576); // 1mb
+define('ALLOWED_TYPES', ['image/jpeg', 'image/png', 'image/gif']);
+define('UPLOAD_DIR', 'uploads');
+define('COMMENT_DIR', 'comments');
